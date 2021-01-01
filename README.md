0e06010acc45926a8223656b271a69d03e6d7ca2
Adam Stevenson <adam.stevenson@e01d.com>
2021-01-01 15:16:18 -0600

    Forked code from the Meadow project, added license, readme, targets, algorithm notes and package information.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Evo.Keccak
-An implementation of the Keccak 
+An implementation of the Keccak-f[1600] algorithm.
+
+This code was originally forked from the [Meadow](https://github.com/MeadowSuite/Meadow/) under the MIT license.  
---
+++ src/Evo.Keccak/StringUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Evo.Keccak
+{
+    public static class StringUtil
+    {
+        public static readonly Encoding UTF8 = new UTF8Encoding(false, false);
+
+        static readonly Regex NewLineRegex = new Regex(@"\r\n|\n\r|\n|\r");
+
+        public static string NormalizeNewLines(string input, string newLine = "\n")
+        {
+            return NewLineRegex.Replace(input, newLine);
+        }
+
+    }
+}
